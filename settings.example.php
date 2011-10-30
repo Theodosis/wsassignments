8695f723f44221d0e3e3aa79654ff1ecdb3cbdf5
Aleksis Brezas <abresas@kamibu.com>
2011-10-30 20:26:07 +0200

    basic auth

---
+++ settings.example.php
@@ -0,0 +1,22 @@
+<?php
+
+    $settings = array(
+        'title' => 'Web Seminar Assignments',
+        'db' => array(
+            'host' => 'localhost',
+            'name' => 'name',
+            'user' => 'user',
+            'pass' => 'pass'
+        ),
+        'users' => array(
+            'user' => array(
+                'password' => 'password',
+                'email' => 'email',
+                'name' => 'name'
+            )
+        )
+    );
+
+    return $settings;
+
+?>
