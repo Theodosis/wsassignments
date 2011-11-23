c56d05e9e2b01b12d81e10299c995872ba06612c
Ted <thsourg@gmail.com>
2011-11-23 14:11:09 +0000

    layout

---
+++ views/footer.php
@@ -0,0 +1,8 @@
+            <div class="eof"></div>
+        </div>
+        <script type="text/javascript" src="/js/jquery-1.6.2.min.js"></script>
+        <script type="text/javascript" src="/js/datatables/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="/js/jquery.jeditable.mini.js"></script>
+        <script type="text/javascript" src="/js/script.js"></script>
+    </body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,15 @@
+<?='<?xml version="1.0" encoding="utf-8"?>'?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="el" lang="el">
+    <head>
+        <title><?= $title; ?></title>
+        <link type="text/css" rel="stylesheet" href="/css/global.css?2" />
+        <link type="text/css" rel="stylesheet" href="/js/datatables/css/demo_table.css?1" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body class="<?php
+        echo $controller;
+    ?>">
+        <div id="container">
+            <h1><?= $title; ?></h1>
+            <span id="exit" title="Έξοδος"></span>
