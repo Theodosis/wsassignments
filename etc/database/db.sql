cbbcf51739db09b4fa51cc7eecd065531889db40
Theodosis Sourgkounis <thsourg@gmail.com>
2011-11-27 11:59:24 +0200

    db schema update

--- etc/database/db.sql
+++ etc/database/db.sql
@@ -1,54 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.3.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 27, 2011 at 09:59 AM
+-- Server version: 5.1.49
+-- PHP Version: 5.3.3-7+squeeze3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `wsthmmy`
+--
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `assignments`
+-- Table structure for table `assignment`
 --
 
-CREATE TABLE IF NOT EXISTS `assignments` (
+CREATE TABLE IF NOT EXISTS `assignment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) NOT NULL,
-  `correctreply` text NOT NULL,
-  `wrongreply` text NOT NULL,
+  `description` varchar(100) COLLATE utf8_bin NOT NULL,
+  `start` date NOT NULL,
+  `end` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `students`
+-- Table structure for table `submission`
 --
 
-CREATE TABLE IF NOT EXISTS `students` (
+CREATE TABLE IF NOT EXISTS `submission` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(256) NOT NULL,
-  `lastname` varchar(256) NOT NULL,
-  `email` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `submissions`
---
-
-CREATE TABLE IF NOT EXISTS `submissions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
   `assignmentid` int(11) NOT NULL,
-  `studentid` int(11) NOT NULL,
-  `iscorrect` tinyint(1) NOT NULL,
-  `isreplied` tinyint(1) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `validationid` int(1) NOT NULL,
+  `comment` text COLLATE utf8_bin,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `assignmentperstudent` (`studentid`,`assignmentid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `userid` (`userid`),
+  KEY `assignmentid` (`assignmentid`),
+  KEY `validationid` (`validationid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) COLLATE utf8_bin NOT NULL,
+  `firstname` varchar(128) COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(128) COLLATE utf8_bin NOT NULL,
+  `email` varchar(128) COLLATE utf8_bin NOT NULL,
+  `password` varchar(32) COLLATE utf8_bin NOT NULL,
+  `authtoken` varchar(32) COLLATE utf8_bin NOT NULL,
+  `rights` int(4) NOT NULL DEFAULT '30',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `validation`
+--
+
+CREATE TABLE IF NOT EXISTS `validation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(128) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `submissions`
+-- Constraints for table `submission`
 --
-ALTER TABLE `submissions`
-  ADD CONSTRAINT `submissions_ibfk_1` FOREIGN KEY (`studentid`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `submission`
+  ADD CONSTRAINT `submission_ibfk_3` FOREIGN KEY (`assignmentid`) REFERENCES `assignment` (`id`),
+  ADD CONSTRAINT `submission_ibfk_4` FOREIGN KEY (`validationid`) REFERENCES `validation` (`id`),
+  ADD CONSTRAINT `submission_ibfk_5` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
+
--- views/submission/listing.php
+++
@@ -1,7 +0,0 @@
-<?php
-    var_dump( $submissions );
-    echo "<br />";
-    var_dump( $offset );
-    echo "<br />";
-    var_dump( $limit );
-?>
